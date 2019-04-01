70ba90e8f7df90ef1c4bb5b1d97f60521a426d56
zenmaster24 <zen.accounts@hotmail.com>
2019-04-01 18:11:50 +1100

    Revert "Adding user-data.yml to install portainer and cockpit. Added hypriot flash script. Add pre-commit hook script."
    This reverts commit 44155bf425f711085b2b33db48706eafca18d64b.

--- flash.sh
+++
@@ -1,8 +0,0 @@
-export VERSION='1.10.0'
-
-// flash it
-flash \
-  --hostname hypriot-${VERSION} \
-  --userdata ./user-data.yml \
-  --device /dev/sdc \
-  ~/Downloads/hypriotos-rpi-v${VERSION}.img.zip
